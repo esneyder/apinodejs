aa32ce7c3975f52dbb8c8f190a9c47899f308828
Esneyder Alvarez <esneyder@MacBook-Pro-de-Esneyder.local>
2019-02-23 22:22:47 -0500

    logger winston

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const logger = require('./utils/logger')
 const productosRouter = require('./api/recursos/productos/productos.route')
+ 
+logger.info('Hola Soy winston')
+logger.debug('Hola Soy debug')
+logger.warn('Hola Soy una advertencia')
+logger.error('Hola Soy un error')
 const app = express()
 
 app.use(bodyParser.json())
-app.use('/productos',productosRouter)
+app.use('/productos', productosRouter)
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send('Api para vender')
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Escuchando el puesto 3000')
 })
